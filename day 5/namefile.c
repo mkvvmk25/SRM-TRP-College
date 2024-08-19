5db1a9d7d287b85b5b66c5a82a6cec547c2be32f
mkvvmk25 <152393796+mkvvmk25@users.noreply.github.com>
2024-08-19 14:42:16 +0530

    Add files via upload

---
+++ day 5/namefile.c
@@ -0,0 +1,63 @@
+// # preprocessing...
+#include<stdio.h>
+// int linearSearchArr(int *ptr, int n, int ele);
+// int linearSearchArr(int*, int, int);
+int linearSearchArr(int *ptr, int n, int ele)
+{
+    for(int i = 0; i < n; i++)
+    {
+        break;
+        if (ptr[i] == ele)
+        {
+            return i; 
+        }
+    }
+    return -1;
+}
+// //          5,5
+// //          4,5
+// //          3,5
+// //         2,5
+// //         1,5
+// //         0,5
+int fun(int *ptr, int i, int n, int ele)
+{
+    if(i == n)
+    {
+        printf("\nend....");
+        return -1;
+    }
+    // printf("hello---> %d", i);
+    if(ptr[i] == ele)
+    {
+        return i; 
+    }
+    int f = fun(ptr, i+1, n, ele);
+    return f;
+}
+
+int main()
+{
+    // searching algo 
+        // linear 
+            // arr will unsorted 
+            // iterative way 
+                // loops 
+            // recurive 
+                // funcs
+        // binary 
+            //  arr will sorted 
+
+    int arr[5] = {4,8,6,1,0};
+    int n = 5; 
+    int seEle = 0; 
+    int ind = fun(arr, 0, n, seEle); 
+    //  0,5
+
+    // int ind = linearSearchArr(arr, n, seEle ); 
+
+    // // int *ptr = arr;
+    printf("%d",ind);
+}
+
+https://bit.ly/WE-190824 
